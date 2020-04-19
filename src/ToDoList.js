8134ca0764aa42b644013453180cc9bcd76e9535
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-04-20 00:50:20 +0545

    A

---
+++ src/ToDoList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Paper from '@material-ui/core/Paper'
+import List from '@material-ui/core/List'
+
+import ToDo from './ToDo'
+
+export default function ToDoList({
+  todoArray,
+  toggleToDo,
+  handleDelete,
+  handleEdit,
+}) {
+  return (
+    <Paper style={{ margin: '1rem 0' }}>
+      {todoArray.map((todo) => (
+        <List>
+          <ToDo
+            item={todo}
+            toggleToDo={toggleToDo}
+            key={todo.id}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+          />
+        </List>
+      ))}
+    </Paper>
+  )
+}
